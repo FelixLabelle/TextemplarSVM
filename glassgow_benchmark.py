6515b889bb471654f0ab84b484b51e24063de93d
Felix Labelle <flx.labelle@gmail.com>
2023-04-16 18:25:33 -0400

    Added benchmark for glassgow dataset, rewrote model code to be more streamlined

---
+++ glassgow_benchmark.py
@@ -0,0 +1,142 @@
+import os
+
+def load_data(path):
+    #_____________ Read data from CISI.ALL file and store in dictinary ________________
+    with open(os.path.join(path, 'CISI.ALL')) as f:
+        lines = ""
+        for l in f.readlines():
+            lines += "\n" + l.strip() if l.startswith(".") else " " + l.strip()
+        lines = lines.lstrip("\n").split("\n")
+ 
+    doc_set = {}
+    doc_id = ""
+    doc_text = ""
+
+    for l in lines:
+        if l.startswith(".I"):
+            doc_id = l.split(" ")[1].strip() 
+        elif l.startswith(".X"):
+            doc_set[doc_id] = doc_text.lstrip(" ")
+            doc_id = ""
+            doc_text = ""
+        else:
+            doc_text += l.strip()[3:] + " " 
+    
+    #_____________ Read data from CISI.QRY file and store in dictinary ________________
+    
+    with open(os.path.join(path, 'CISI.QRY')) as f:
+        lines = ""
+        for l in f.readlines():
+            lines += "\n" + l.strip() if l.startswith(".") else " " + l.strip()
+        lines = lines.lstrip("\n").split("\n")
+          
+    qry_set = {}
+    qry_id = ""
+    for l in lines:
+        if l.startswith(".I"):
+            qry_id = l.split(" ")[1].strip() 
+        elif l.startswith(".W"):
+            qry_set[qry_id] = l.strip()[3:]
+            qry_id = ""
+    
+    
+    #_____________ Read data from CISI.REL file and store in dictinary ________________
+    
+    rel_set = {}
+    with open(os.path.join(path, 'CISI.REL')) as f:
+        for l in f.readlines():
+            qry_id = l.lstrip(" ").strip("\n").split("\t")[0].split(" ")[0] 
+            doc_id = l.lstrip(" ").strip("\n").split("\t")[0].split(" ")[-1]
+
+            if qry_id in rel_set:
+                rel_set[qry_id].append(doc_id)
+            else:
+                rel_set[qry_id] = []
+                rel_set[qry_id].append(doc_id)
+    
+    doc_set = {int(id):doc for (id,doc) in doc_set.items()}
+    qry_set = {int(id):qry for (id,qry) in qry_set.items()}
+    rel_set = {int(qid):list(map(int, did_lst)) for (qid,did_lst) in rel_set.items()}
+    
+    return doc_set, qry_set, rel_set
+    
+if __name__ == "__main__":
+    from collections import defaultdict
+    
+    from random import seed
+    from retrieval_models import CosineSimilarityRetrieval, SVMRetrieval
+    from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
+    from tqdm.auto import tqdm
+    from sentence_transformers import SentenceTransformer
+
+    seed(0)
+    
+    doc_mapping,query_mapping,query_doc_mapping = load_data('glassgow_ir_dataset')
+    
+    num_mappings = len([relationship for relationships in query_doc_mapping.values() for relationship in relationships])
+    assert(all([key == idx+1 for idx,key in enumerate(doc_mapping.keys())]))
+    assert(all([key == idx+1 for idx,key in enumerate(query_mapping.keys())]))
+    
+    # Create embeddings
+    MAX_VOCAB_SIZE = 30_000
+    #vectorizer = CountVectorizer(lowercase=True,max_features=MAX_VOCAB_SIZE)
+    '''
+    vectorizer = TfidfVectorizer(lowercase=True,max_features=MAX_VOCAB_SIZE)
+    corpus_embeddings = vectorizer.fit_transform(doc_mapping.values())
+    
+    #retrieval_model = CosineSimilarityRetrieval(lambda x: vectorizer.transform(x), corpus_embeddings=corpus_embeddings)
+    #retrieval_model = SVMRetrieval(lambda x: vectorizer.transform(x), "linear", corpus_embeddings=corpus_embeddings)
+    retrieval_model = SVMRetrieval(lambda x: vectorizer.transform(x), "precomputed", corpus_embeddings=corpus_embeddings)
+    '''
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    corpus_embeddings = model.encode([doc for doc in doc_mapping.values()],batch_size=1,show_progress_bar =True,convert_to_numpy =True)
+    
+    #retrieval_model = CosineSimilarityRetrieval(lambda x: model.encode(x), corpus_embeddings=corpus_embeddings)
+    #retrieval_model = SVMRetrieval(lambda x: model.encode(x), "linear", corpus_embeddings=corpus_embeddings)
+    retrieval_model = SVMRetrieval(lambda x: model.encode(x), "precomputed", corpus_embeddings=corpus_embeddings)
+    
+    metrics = []
+    for query_idx, query in tqdm(query_mapping.items()):
+
+        relevant_document_idxs = query_doc_mapping.get(query_idx,[])
+        # Skip empty documents, otherwise this impacts the metrics' meaningfulness
+        if not relevant_document_idxs:
+            continue
+        _,cosine_sim_sorted_idxs = retrieval_model.query([query])
+        cosine_sim_sorted_idxs = cosine_sim_sorted_idxs +1
+        # METRICS
+        reported_metrics = {}
+        for k in [1,2,5,10,20]:
+            top_k_preds = cosine_sim_sorted_idxs[:k]
+            tp = sum([idx in relevant_document_idxs for idx in top_k_preds])
+            fp = k - tp
+            fn = len(relevant_document_idxs) - tp
+            
+            precision_at_k = tp / (tp+fp)
+            try:
+                recall_at_k = tp / min((tp+fn),k)
+            except:
+                recall_at_k = 0
+            reported_metrics[f"precision_at_{k}"] = precision_at_k
+            reported_metrics[f"recall_at_{k}"] = recall_at_k
+            reported_metrics[f"result_at_{k}"] = tp > 0
+        metrics.append(reported_metrics)
+    
+    mean_average_metrics = defaultdict(float)
+    for item in metrics:
+        for key in metrics[0].keys():
+            mean_average_metrics[key] += item[key]/len(metrics)
+    
+    print(mean_average_metrics)
+    # "Ablations" to run
+    # 1. Embeddings (count based, tf idf, sentence embedding)
+    # 2. Model (for sentence embedding)
+    
+    
+    # 3. Sample size (for svm based models)
+    
+    # load models
+    # run a query 
+    # for each query evaluate
+    # report scores
+    # report times
---
+++ retrieval_models.py
@@ -0,0 +1,155 @@
+import random
+
+import numpy as np
+from scipy.sparse import vstack
+from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
+from sklearn.metrics.pairwise import cosine_similarity
+
+'''
+# Format data
+l2_svm_topk = [search_corpus[i] for i in sorted_idx[:TOP_K]]
+
+# QUESTION: What are the effects of having the query in the dataset? For SVM does
+# this cause issues?
+# https://www.cs.cmu.edu/~tmalisie/projects/iccv11/
+# Is this a problem?
+# Cosine Similarity
+'''
+class CosineSimilarityRetrieval:
+    def __init__(self, vectorizer,corpus=None,corpus_embeddings=None):
+        self.vectorizer = vectorizer
+        if type(corpus_embeddings) != None:
+            self.corpus_embeddings = corpus_embeddings
+        elif type(corpus) != None:
+            corpus_embeddings = self.vectorizer(corpus)
+            self.corpus_embeddings = corpus_embeddings
+        else:
+            raise ValueError("No corpus or corpus embeddings passed in")
+    
+    def query(self, query_doc):
+        query_embedding = self.vectorizer(query_doc)
+        similarities = cosine_similarity(query_embedding, self.corpus_embeddings)
+        sorted_idx = (-similarities).squeeze(0).argsort()
+        return similarities, sorted_idx
+
+class SVMRetrieval:
+    def __init__(self, vectorizer,kernel="linear",corpus=None,corpus_embeddings=None, sample_size=-1):
+        self.vectorizer = vectorizer
+        self.kernel = kernel
+        
+        if type(corpus_embeddings) != None:
+            self.corpus_embeddings = corpus_embeddings
+        elif type(corpus) != None:
+            corpus_embeddings = self.vectorizer(corpus)
+            self.corpus_embeddings = corpus_embeddings
+        else:
+            raise ValueError("No corpus or corpus embeddings passed in")
+        
+        # todo: pass in HPs
+        if sample_size > 0:
+            sample_idxs = random.sample(range(0,self.corpus_embeddings.shape[0]),k=sample_size)
+            self.train_embeddings = self.corpus_embeddings[sample_idxs,:]
+        else:
+            self.train_embeddings = self.corpus_embeddings
+         
+        if self.kernel == "linear":
+            self.model = LinearSVC(class_weight='balanced',verbose=False, max_iter=10000, tol=1e-6, C=0.1)
+        elif self.kernel == "precomputed":
+            self.model = SVC(kernel="precomputed",class_weight='balanced',verbose=False, max_iter=10000, tol=1e-6, C=0.1)
+        elif self.kernel == "cosine_sim":
+            self.model = SVC(kernel=cosine_similarity,class_weight='balanced',verbose=False, max_iter=10000, tol=1e-6, C=0.1)
+        else:
+            raise NotImplementedError(f"Kernel {kernel} is not currently supported")
+            
+        self.labels = np.zeros(self.train_embeddings.shape[0]+1)
+        self.labels[0] = 1
+        
+    def query(self, query_doc):
+        query_embedding = self.vectorizer(query_doc)
+        concat_query_corpus_embeddings = vstack([query_embedding, self.train_embeddings])
+        
+        if self.kernel == "precomputed":
+            training_embeddings = cosine_similarity(concat_query_corpus_embeddings)
+        else:
+            training_embeddings = concat_query_corpus_embeddings
+            
+        self.model.fit(training_embeddings,self.labels)
+        
+        if self.kernel == "precomputed":
+            corpus_embeddings = cosine_similarity(self.corpus_embeddings,concat_query_corpus_embeddings)
+        else:
+            corpus_embeddings = self.corpus_embeddings
+            
+        similarities = self.model.decision_function(corpus_embeddings)
+        sorted_idx = np.argsort(-similarities)
+        return similarities, sorted_idx
+        
+# Cosine Similarity SVM
+'''
+model = SVC(kernel=cosine_similarity,class_weight='balanced',verbose=False, max_iter=10000, tol=1e-6, C=0.1)
+SVM_SAMPLE_SIZE = 1000
+sampled_idxs = [0] + random.sample(range(1,concat_query_corpus_embeddings.shape[0]),k=SVM_SAMPLE_SIZE)
+model.fit(concat_query_corpus_embeddings[sampled_idxs,:],labels[sampled_idxs])
+similarities = model.decision_function(corpus_embeddings)
+sorted_idx = np.argsort(-similarities)
+cosine_svm_topk = [search_corpus[i] for i in sorted_idx[:TOP_K]]
+
+print(l2_svm_topk)
+print(cosine_sim_topk)
+print(cosine_svm_topk)
+
+# Cosine Similarity SVM
+model = SVC(kernel="precomputed",class_weight='balanced',verbose=False, max_iter=10000, tol=1e-6, C=0.1)
+SVM_SAMPLE_SIZE = 1000
+sampled_idxs = [0] + random.sample(range(1,concat_query_corpus_embeddings.shape[0]),k=SVM_SAMPLE_SIZE)
+cosine_sim_distances  = cosine_similarity(concat_query_corpus_embeddings[sampled_idxs,:])
+model.fit(cosine_sim_distances,labels[sampled_idxs])
+dev_sim = cosine_similarity(corpus_embeddings,concat_query_corpus_embeddings[sampled_idxs,:])
+similarities = model.decision_function(dev_sim)
+sorted_idx = np.argsort(-similarities)
+cosine_distance_svm_topk = [search_corpus[i] for i in sorted_idx[:TOP_K]]
+'''
+if __name__ == "__main__":
+    from sklearn.feature_extraction.text import CountVectorizer
+    random.seed(0)
+    
+    with open('babylm_dev_v1.dev','r',encoding="utf-8") as fh:
+        search_corpus = list(map(lambda x: x.strip(), fh.readlines()))
+
+    TOP_K = 20
+
+    # Create embeddings
+    MAX_VOCAB_SIZE = 30_000
+    vectorizer = CountVectorizer(lowercase=True,max_features=MAX_VOCAB_SIZE)
+    corpus_embeddings = vectorizer.fit_transform(search_corpus)
+    
+    # NOTE: Make sure the query is the format expected by your vectorizer
+    query = [search_corpus[0]]
+
+    cosine_sim_search = CosineSimilarityRetrieval(lambda x: vectorizer.transform(x), corpus_embeddings=corpus_embeddings)
+    _,cosine_sim_sorted_idxs = cosine_sim_search.query(query)
+    cosine_sim_topk = [search_corpus[idx] for idx in cosine_sim_sorted_idxs[:TOP_K]]
+
+    l2_svm_search = SVMRetrieval(lambda x: vectorizer.transform(x), "linear", corpus_embeddings=corpus_embeddings)
+    _,l2_svm_sorted_idxs = l2_svm_search.query(query)
+    l2_svm_topk = [search_corpus[idx] for idx in l2_svm_sorted_idxs[:TOP_K]]
+
+    cosine_svm_search = SVMRetrieval(lambda x: vectorizer.transform(x), "cosine_sim", corpus_embeddings=corpus_embeddings,sample_size=1000)
+    _,cosine_svm_sorted_idxs = cosine_svm_search.query(query)
+    cosine_svm_topk = [search_corpus[idx] for idx in cosine_svm_sorted_idxs[:TOP_K]]
+    
+    cosine_distance_svm_search = SVMRetrieval(lambda x: vectorizer.transform(x), "precomputed", corpus_embeddings=corpus_embeddings,sample_size=1000)
+    _,cosine_distance_svm_sorted_idxs = cosine_distance_svm_search.query(query)
+    cosine_distance_svm_topk = [search_corpus[idx] for idx in cosine_distance_svm_sorted_idxs[:TOP_K]]
+    
+    print("L2 SVM results")
+    print(l2_svm_topk)
+    print("Cosine SVM results")
+    print(cosine_sim_topk)
+    #print("Cosine Kernel SVM results")
+    #print(cosine_svm_topk)
+    print("Cosine Distance SVM results")
+    print(cosine_distance_svm_topk)
+
+
